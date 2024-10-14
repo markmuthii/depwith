0741030dfbe485969418ddd841d47307401bd1cd
Mark Muthii <mark.muthii@strathmore.edu>
2024-10-14 16:15:30 +0300

    Undo demonstration code

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,17 +1,5 @@
-export const getRegisterUser = (req, res) => {
-  res.send("GET register endpoint");
-};
-
-export const postRegisterUser = (req, res) => {
+export const registerUser = (req, res) => {
   res.send("POST register endpoint");
-};
-
-export const putRegisterUser = (req, res) => {
-  res.send("PUT register endpoint");
-};
-
-export const deleteRegisterUser = (req, res) => {
-  res.send("DELETE register endpoint");
 };
 
 export const loginUser = (req, res) => {
--- server/src/routes/v1/auth.js
+++ server/src/routes/v1/auth.js
@@ -1,16 +1,10 @@
 import { Router } from "express";
-import {
-  deleteRegisterUser,
-  getRegisterUser,
-  loginUser,
-  postRegisterUser,
-  putRegisterUser,
-} from "../../controllers/auth.js";
+import { loginUser, registerUser } from "../../controllers/auth.js";
 
 const authRouter = Router();
 
 // /api/v1/auth/register
-authRouter.post("/register", postRegisterUser);
+authRouter.post("/register", registerUser);
 
 // /api/v1/auth/login
 authRouter.post("/login", loginUser);
