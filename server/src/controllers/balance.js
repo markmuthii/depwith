6c66c339d352c1eebfebab1a579aaf6ddd0f3657
Mark Muthii <mark.muthii@strathmore.edu>
2024-10-23 15:07:35 +0300

    Implement get user balance logic

---
+++ server/src/controllers/balance.js
@@ -0,0 +1,38 @@
+import { isValidObjectId } from "mongoose";
+import { Balance } from "../database/Models/Balance.js";
+
+// TODO: When roles are added, make sure that if the loggedInUser is not the same as the user whose balance is being requested (userWeAreInterestedIn), then the logged in user must have the authority to view other users' balances
+export const getUserBalance = async (req, res) => {
+  try {
+    // {
+    //   userId: "", // ID of the user making the request (user who is logged in)
+    //   params: {
+    //     userId: "" // ID of the user we want to get the balance of
+    //   }
+    // }
+    const { userId: loggedInUser } = req;
+    const { userId: userWeAreInterestedIn } = req.params;
+
+    if (!isValidObjectId(userWeAreInterestedIn)) {
+      return res.status(400).json({
+        message: "Deformed ID Passed",
+      });
+    }
+
+    const userBalance = await Balance.findOne({ user: userWeAreInterestedIn });
+
+    if (!userBalance) {
+      return res.status(400).json({
+        message: "User with that ID does not exist",
+      });
+    }
+
+    res.json({
+      balance: userBalance.balance, // IF there is a userBalance.balance (it is defined), then use that. If not, then 0
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
